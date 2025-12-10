d1acebba79d4b8cdacc6e64819f0b108e2770ec9
skroheedali <skroheedali@gmail.com>
2025-12-10 12:50:27 +0530

    Git test 2

--- src/Ali.java
+++ src/Ali.java
@@ -1,7 +1,7 @@
 
 public class Ali {
 	public static void main(String[] args) {
-		System.out.println("Ali"); 
+		System.out.println("A l i"); 
 		
 	}
 
---
+++ src/ArifSir.java
@@ -0,0 +1,8 @@
+
+public class ArifSir {
+
+	public static void main(String[] args) {
+		System.out.println("Arif");
+	}
+
+}
